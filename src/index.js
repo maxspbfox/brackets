040c242b57e871c23200d941f2386d80a26ddea2
Maxim Zugan <maxspbfox@gmail.com>
2024-08-05 12:27:46 -0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let oldStringLength;
+
+  do {
+    oldStringLength = str.length;
+
+    bracketsConfig.forEach((config) => {
+      str = str.replaceAll(config.join(""), "");
+    });
+  } while (str.length < oldStringLength);
+
+  return str.length === 0;
+};
